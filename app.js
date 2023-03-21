5eabf574c4758b14f45a5dcfc5a54e4e21e5b6de
Maylis <maylis.ameto@gmail.com>
2023-03-21 15:48:54 +0100

    Accept incoming changes

--- app.js
+++ app.js
@@ -2,11 +2,7 @@
 // let image = document.getElementById('image');
 // let name = document.getElementById('name');
 // Scraping => thème  
-<<<<<<< HEAD
-// title, year,  
-=======
 // Exemple data : title, year,
->>>>>>> 61870c65e74975371f320e90e6e073c94df0d128
 
 const requestApi = async () => {
   let requestString = `https://imdb-api.com/en/API/SearchMovie/k_5lsv0goy`;
